4384ddbd3433f3f104241f6ade35fa14d30da998
davide <davidetacchini11@gmail.com>
2020-10-15 08:59:54 +0200

    Added color transition to navbar items

--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -10,10 +10,16 @@
     </template>
 
     <template slot="start">
-      <b-navbar-item tag="router-link" :to="{ path: '/commands' }"
+      <b-navbar-item
+        tag="router-link"
+        :to="{ path: '/commands' }"
+        class="navbar-item"
         >Commands</b-navbar-item
       >
-      <b-navbar-item tag="router-link" :to="{ path: '/status' }"
+      <b-navbar-item
+        tag="router-link"
+        :to="{ path: '/status' }"
+        class="navbar-item"
         >Status</b-navbar-item
       >
       <b-navbar-item
@@ -96,3 +102,9 @@ export default {
   },
 };
 </script>
+
+<style scoped lang="scss">
+.navbar-item {
+  transition: color 0.2s ease-in-out;
+}
+</style>
