87ddb0f4b2ef44c11df2094e903e8ee1078cc2c0
davide <davidetacchini11@gmail.com>
2020-10-22 18:37:50 +0200

    Updated

---
+++ src/components/NotAvailable.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <section class="hero is-dark is-medium">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title is-2 is-spaced">
+            {{ endpoint }}
+          </h1>
+          <h2 class="subtitle">
+            We are working hard to make this page available as soon as possible!
+          </h2>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "not-available",
+
+  props: {
+    endpoint: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.container {
+  max-width: 450px !important;
+  margin: 0 auto !important;
+  text-align: center !important;
+}
+
+@media only screen and (max-width: 768px) {
+  h1 {
+    font-size: 2rem !important;
+  }
+}
+</style>
--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,77 +1,15 @@
 <template>
   <div>
-    <section class="hero is-dark">
-      <div class="hero-body">
-        <div class="container">
-          <div class="box countdown has-text-white">
-            This page automatically refreshes every 30 seconds. Next update in:
-            {{ countDown }}
-          </div>
-          <div class="columns is-multiline is-mobile">
-            <status-card title="Bot Status" :data="statistics" />
-            <status-card title="Host Status" :data="host" />
-          </div>
-          <div class="columns">
-            <shards-card title="Shards" :data="shards" />
-          </div>
-        </div>
-      </div>
-    </section>
+    <not-available endpoint="Status" />
   </div>
 </template>
 
 <script>
-import StatusCard from "@/components/status/StatusCard";
-import ShardsCard from "@/components/status/ShardsCard";
+import NotAvailable from "@/components/NotAvailable";
 
 export default {
-  data: () => {
-    return {
-      countDown: 30,
-    };
-  },
-
   components: {
-    StatusCard,
-    ShardsCard,
-  },
-
-  computed: {
-    statistics() {
-      return this.$store.getters.statistics;
-    },
-    host() {
-      return this.$store.getters.host;
-    },
-    shards() {
-      return this.$store.getters.shards;
-    },
-  },
-
-  created() {
-    this.$store.dispatch("getStats");
-  },
-
-  watch: {
-    countDown: {
-      handler(value) {
-        if (value > 0) {
-          setTimeout(() => {
-            this.countDown--;
-          }, 1000);
-        } else {
-          this.$store.dispatch("getStats");
-          this.countDown = 30;
-        }
-      },
-      immediate: true,
-    },
+    NotAvailable,
   },
 };
 </script>
-
-<style scoped lang="scss">
-.countdown {
-  font-size: 0.875rem !important;
-}
-</style>
