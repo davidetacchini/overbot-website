352b4f9ec00a74a807b04a76c34da6c592fcf970
davide <davidetacchini11@gmail.com>
2020-11-16 18:52:25 +0100

    Added back status view

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -1,15 +1,72 @@
 <template>
   <div>
-    <not-available endpoint="Status" />
+    <section class="hero is-dark">
+      <div class="hero-body">
+        <div class="container">
+          <div class="box countdown has-text-white">
+            This page automatically refreshes every 30 seconds. Next update in:
+            {{ countDown }}
+          </div>
+          <div class="columns is-multiline is-mobile">
+            <status-card title="Bot Status" :data="bot" />
+            <status-card title="Host Status" :data="host" />
+          </div>
+          <div class="columns">
+            <shards-card title="Shards" :data="shards" />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-import NotAvailable from "@/components/NotAvailable";
-
+import StatusCard from "@/components/status/StatusCard";
+import ShardsCard from "@/components/status/ShardsCard";
 export default {
+  data: () => {
+    return {
+      countDown: 30,
+    };
+  },
   components: {
-    NotAvailable,
+    StatusCard,
+    ShardsCard,
+  },
+  computed: {
+    bot() {
+      return this.$store.getters.bot;
+    },
+    host() {
+      return this.$store.getters.host;
+    },
+    shards() {
+      return this.$store.getters.shards;
+    },
+  },
+  created() {
+    this.$store.dispatch("getStats");
+  },
+  watch: {
+    countDown: {
+      handler(value) {
+        if (value > 0) {
+          setTimeout(() => {
+            this.countDown--;
+          }, 1000);
+        } else {
+          this.$store.dispatch("getStats");
+          this.countDown = 30;
+        }
+      },
+      immediate: true,
+    },
   },
 };
 </script>
+
+<style scoped lang="scss">
+.countdown {
+  font-size: 0.875rem !important;
+}
+</style>
