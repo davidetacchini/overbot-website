86883d0a62740ad0669e8f0ae37678c3fb7ebcbd
davide <davidetacchini11@gmail.com>
2020-11-26 22:46:26 +0100

    Fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.VUE_APP_OVERCORD_API_URL,
+  baseURL: process.env.VUE_APP_OVERBOT_API_URL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: process.env.VUE_APP_SECRET_ACCESS_TOKEN,
