31a54439b531647c5c48459678901dd2c4ea5634
davide <davidetacchini11@gmail.com>
2021-03-08 16:36:16 +0100

    Renamed CustomButton to BaseButton

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <a
+    :href="link"
+    target="_blank"
+    :class="['button is-fullwidth', color, { 'is-stretched': isStretched }]"
+  >
+    <span class="button__content">{{ action }}</span>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'base-button',
+
+  props: {
+    action: String,
+    color: String,
+    link: String,
+    isStretched: Boolean,
+  },
+};
+</script>
+
+<style scoped lang="scss"></style>
