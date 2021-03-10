300065bc75bf1eb28b590e173c5e5b810630d578
davide <davidetacchini11@gmail.com>
2021-03-10 20:53:46 +0100

    Update ErrorMessage component

--- src/components/ErrorMessage.vue
+++ src/components/ErrorMessage.vue
@@ -2,12 +2,9 @@
   <article class="message is-danger">
     <div class="message-body">
       <p>
-        Ops! Something bad happened. Please refresh the page.
-        <a href="https://discord.gg/8g3jnxv" target="_blank" rel="noopener"> Support Server</a>
-        •
-        <a href="https://github.com/davidetacchini/overbot-website" target="_blank" rel="noopener"
-          >GitHub issues</a
-        >
+        Ops! Something bad happened. Please refresh the page. If this error persists, contact the
+        support server on
+        <a href="https://discord.gg/8g3jnxv" target="_blank" rel="noopener">Discord.</a>
       </p>
     </div>
   </article>
@@ -22,16 +19,9 @@ export default {
 <style scoped lang="scss">
 .message {
   margin: 25px auto !important;
-  max-width: 100% !important;
 
   p {
     font-size: 0.875rem !important;
-  }
-}
-
-@media screen and (max-width: 800px) {
-  .message {
-    margin: 0 20px !important;
   }
 }
 </style>
