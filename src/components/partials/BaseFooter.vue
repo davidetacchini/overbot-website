e3839a785012c8147771746a2a28d60b87e0a2b4
davide <davidetacchini11@gmail.com>
2021-03-11 20:19:45 +0100

    Code refactor

--- src/components/partials/BaseFooter.vue
+++ src/components/partials/BaseFooter.vue
@@ -4,69 +4,33 @@
       <div class="columns is-multiline is-mobile">
         <div class="column">
           <h1 class="title is-4 has-text-white">OverBot</h1>
-          <p class="has-text-grey">
-            Copyright &copy; {{ currentYear }} OverBot. All rights reserved.
-          </p>
-          <p class="has-text-grey">
-            OverBot is not affiliated with Blizzard Entertainment.
-          </p>
-          <p class="has-text-grey">
-            Made by
-            <a
-              href="https://github.com/davidetacchini"
-              target="_blank"
-              rel="noopener"
-              class="link has-text-primary"
-              >Davide</a
-            >
-          </p>
-          <a href="https://github.com/davidetacchini/overbot" target="_blank" rel="noopener">
-            <img
-              alt="GitHub Repo Stars"
-              src="https://img.shields.io/github/stars/davidetacchini/overbot?style=social"
-            />
-          </a>
+          <p>Copyright &copy; {{ currentYear }} OverBot. All rights reserved.</p>
+          <p>OverBot is not affiliated with Blizzard Entertainment.</p>
         </div>
         <div class="column is-full-mobile">
           <div class="columns is-multiline is-mobile is-full-tablet">
             <div class="column">
               <h1 class="title is-6 has-text-white">Navigation</h1>
-              <router-link to="/commands" class="link">Commands</router-link>
-              <br />
-              <router-link to="/permissions" class="link">
-                Permissions
+              <router-link v-for="(value, key) in navigation" :key="key" :to="value" class="link">
+                {{ key }}
               </router-link>
-              <br />
-              <router-link to="/premium" class="link">Premium</router-link>
-              <br />
-              <router-link to="/status" class="link">Status</router-link>
             </div>
             <div class="column is-one-third-desktop is-full-mobile">
               <h1 class="title is-6 has-text-white">Resources</h1>
               <a
-                href="https://www.flaticon.com/authors/freepik"
+                v-for="(value, key) in resources"
+                :key="key"
+                :href="value"
                 target="_blank"
                 rel="noopener"
                 class="link"
-                >Freepik</a
+                >{{ key }}</a
               >
-              <br />
-              <a href="https://www.flaticon.com/" target="_blank" rel="noopener" class="link"
-                >Flaticon</a
-              >
-              <br />
-              <a href="https://ow-api.com/" target="_blank" rel="noopener" class="link">Ow-API</a>
-              <br />
-              <a href="https://undraw.co/" target="_blank" rel="noopener" class="link">unDraw</a>
             </div>
             <div class="column is-one-third-desktop is-full-mobile">
               <h1 class="title is-6 has-text-white">Legal</h1>
-              <router-link to="/privacy" class="link">
-                Privacy Policy
-              </router-link>
-              <br />
-              <router-link to="/terms" class="link">
-                Terms of Service
+              <router-link v-for="(value, key) in legal" :key="key" :to="value" class="link">
+                {{ key }}
               </router-link>
             </div>
           </div>
@@ -80,6 +44,27 @@
 export default {
   name: 'BaseFooter',
 
+  data: () => {
+    return {
+      navigation: {
+        Commands: '/commands',
+        Permissions: '/permissions',
+        Premium: '/premium',
+        Status: '/status',
+      },
+      resources: {
+        Freepik: 'https://www.flaticon.com/authors/freepik',
+        Flaticon: 'https://www.flaticon.com/',
+        'Ow-API': 'https://ow-api.com/',
+        unDraw: 'https://undraw.co/',
+      },
+      legal: {
+        'Privacy Policy': '/privacy',
+        'Terms of Service': '/terms',
+      },
+    };
+  },
+
   computed: {
     currentYear: () => {
       return new Date().getFullYear();
@@ -90,8 +75,8 @@ export default {
 
 <style scoped lang="scss">
 p {
-  font-size: 14px;
-  color: $white-opacity-lightner-5;
+  font-size: 14px !important;
+  color: $white-opacity-lightner-5 !important;
 }
 
 .content {
@@ -100,10 +85,10 @@ p {
 }
 
 .link {
+  display: block;
   color: $white-opacity-lightner-5;
   font-size: 14px;
   transition: color 175ms ease-out;
-  display: inline-block;
 }
 
 .link:not(:first-of-type) {
--- src/components/partials/Navbar.vue
+++ src/components/partials/Navbar.vue
@@ -7,18 +7,10 @@
     </template>
 
     <template slot="start">
-      <b-navbar-item tag="router-link" to="/commands" class="navbar-item">
-        Commands
+      <b-navbar-item v-for="(value, key) in routes" :key="key" :to="value" tag="router-link">
+        {{ key }}
       </b-navbar-item>
-      <b-navbar-item tag="router-link" to="/permissions" class="navbar-item">
-        Permissions
-      </b-navbar-item>
-      <b-navbar-item tag="router-link" to="/premium" class="navbar-item">
-        Premium
-      </b-navbar-item>
-      <b-navbar-item tag="router-link" to="/status" class="navbar-item">
-        Status
-      </b-navbar-item>
+
       <b-dropdown v-model="navigation" position="is-bottom-left" aria-role="menu" expanded>
         <a class="navbar-item" slot="trigger" role="button">
           <div class="dropdown-more">
@@ -29,37 +21,15 @@
           </div>
         </a>
 
-        <b-dropdown-item custom class="menu-title"> Discord </b-dropdown-item>
-
-        <b-dropdown-item has-link aria-role="menuitem" class="menu-item">
-          <a href="https://top.gg/bot/547546531666984961" target="_blank" rel="noopener"> top.gg</a>
-        </b-dropdown-item>
-        <b-dropdown-item has-link aria-role="menuitem" class="menu-item">
-          <a
-            href="https://discordbotlist.com/bots/547546531666984961"
-            target="_blank"
-            rel="noopener"
-            >discordbotlist.com
-          </a>
-        </b-dropdown-item>
-
-        <b-dropdown-item has-link aria-role="menuitem" class="menu-item">
-          <a href="https://discord.bots.gg/bots/547546531666984961" target="_blank" rel="noopener">
-            discord.bots.gg
-          </a>
-        </b-dropdown-item>
-
-        <b-dropdown-item custom class="menu-title">Social</b-dropdown-item>
-
-        <b-dropdown-item has-link aria-role="menuitem" class="menu-item">
-          <a href="https://github.com/davidetacchini/overbot" target="_blank" rel="noopener">
-            GitHub
-          </a>
-        </b-dropdown-item>
-        <b-dropdown-item has-link aria-role="menuitem" class="menu-item">
-          <a href="https://discord.gg/8g3jnxv" target="_blank" rel="noopener">
-            Support Server
-          </a>
+        <b-dropdown-item
+          v-for="(value, key) in dropdownRoutes"
+          :key="key"
+          :custom="value.isTitle"
+          :has-link="!value.isTitle"
+          :class="[value.isTitle ? 'menu-title' : 'menu-item']"
+        >
+          <a v-if="!value.isTitle" :href="value" target="_blank" rel="noopener">{{ key }}</a>
+          <span v-else>{{ key }}</span>
         </b-dropdown-item>
       </b-dropdown>
     </template>
@@ -73,6 +43,25 @@ export default {
   data: () => {
     return {
       navigation: 'home',
+      routes: {
+        Commands: '/commands',
+        Permissions: '/permissions',
+        Premium: '/premium',
+        Status: '/status',
+      },
+      dropdownRoutes: {
+        Discord: {
+          isTitle: true,
+        },
+        'top.gg': 'https://top.gg/bot/547546531666984961',
+        'discordbotlist.com': 'https://discordbotlist.com/bots/547546531666984961',
+        'discord.bots.gg': 'https://discord.bots.gg/bots/547546531666984961',
+        Social: {
+          isTitle: true,
+        },
+        GitHub: 'https://github.com/davidetacchini/overbot',
+        'Support Server': 'https://discord.gg/8g3jnxv',
+      },
     };
   },
 };
