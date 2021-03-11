9c962c7fe55981781777d372c027270be2540690
davide <davidetacchini11@gmail.com>
2021-03-11 18:42:31 +0100

    Update classes

--- src/components/status/Shard.vue
+++ src/components/status/Shard.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     :class="[
-      'box shard has-text-dark has-background-success',
+      'box has-text-dark has-background-success',
       {
         'has-background-warning': value.latency >= 150 && value.latency < 1000,
       },
@@ -15,7 +15,7 @@
       Latency:
       <span class="has-text-weight-bold">{{ value.latency }}ms</span>
       <br />
-      Server count:
+      Servers:
       <span class="has-text-weight-bold">{{ value.guild_count }}</span>
     </p>
   </div>
@@ -36,7 +36,7 @@ p {
   font-size: 0.875rem;
 }
 
-.shard {
+.box {
   padding: 10px !important;
 }
 </style>
