3d77010507df7d0bad56ae8c85bef89e0c2a660a
davidetacchini <davidetacchini11@gmail.com>
2021-06-12 21:35:10 +0200

    wrap navabar inside container

--- src/components/layout/TheNavbar.vue
+++ src/components/layout/TheNavbar.vue
@@ -1,44 +1,46 @@
 <template>
-  <b-navbar class="is-spaced">
-    <template slot="brand">
-      <b-navbar-item tag="router-link" to="/">
-        <img
-          v-if="$route.path !== '/premium'"
-          :src="require(`@/assets/logos/default.png`)"
-          alt="Logo"
-        />
-        <img v-else :src="require(`@/assets/logos/premium.png`)" alt="Premium Logo" />
-      </b-navbar-item>
-    </template>
+  <div class="container">
+    <b-navbar class="is-spaced">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" to="/">
+          <img
+            v-if="$route.path !== '/premium'"
+            :src="require(`@/assets/logos/default.png`)"
+            alt="Logo"
+          />
+          <img v-else :src="require(`@/assets/logos/premium.png`)" alt="Premium Logo" />
+        </b-navbar-item>
+      </template>
 
-    <template slot="start">
-      <b-navbar-item v-for="(value, key) in routes" :key="key" :to="value" tag="router-link">
-        {{ key }}
-      </b-navbar-item>
+      <template slot="start">
+        <b-navbar-item v-for="(value, key) in routes" :key="key" :to="value" tag="router-link">
+          {{ key }}
+        </b-navbar-item>
 
-      <b-dropdown v-model="navigation" position="is-bottom-left" aria-role="menu" expanded>
-        <a slot="trigger" class="navbar-item" role="button">
-          <div class="dropdown-more">
-            <span>More</span>
-            <span class="icon">
-              <i class="fas fa-chevron-down" />
-            </span>
-          </div>
-        </a>
+        <b-dropdown v-model="navigation" position="is-bottom-left" aria-role="menu" expanded>
+          <a slot="trigger" class="navbar-item" role="button">
+            <div class="dropdown-more">
+              <span>More</span>
+              <span class="icon">
+                <i class="fas fa-chevron-down" />
+              </span>
+            </div>
+          </a>
 
-        <b-dropdown-item
-          v-for="(value, key) in dropdownRoutes"
-          :key="key"
-          :custom="value.isTitle"
-          :has-link="!value.isTitle"
-          :class="[value.isTitle ? 'menu-title' : 'menu-item']"
-        >
-          <a v-if="!value.isTitle" :href="value" target="_blank" rel="noopener">{{ key }}</a>
-          <span v-else>{{ key }}</span>
-        </b-dropdown-item>
-      </b-dropdown>
-    </template>
-  </b-navbar>
+          <b-dropdown-item
+            v-for="(value, key) in dropdownRoutes"
+            :key="key"
+            :custom="value.isTitle"
+            :has-link="!value.isTitle"
+            :class="[value.isTitle ? 'menu-title' : 'menu-item']"
+          >
+            <a v-if="!value.isTitle" :href="value" target="_blank" rel="noopener">{{ key }}</a>
+            <span v-else>{{ key }}</span>
+          </b-dropdown-item>
+        </b-dropdown>
+      </template>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
