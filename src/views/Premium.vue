10c6981ed37b4a066341a4f13793d3afbbad77de
davidetacchini <davidetacchini11@gmail.com>
2022-08-19 18:07:32 +0200

    update /premium to be more intuitive and better looking

--- src/views/Premium.vue
+++ src/views/Premium.vue
@@ -6,9 +6,17 @@
         OverBot is a project founded and maintained by a single person.<br />
         If you'd like to support this project, consider upgrading to one of the Premium plans.
       </h2>
-      <a href="#plans" class="button is-white is-outlined mt-5 p-5">View our plans</a>
+      <div class="columns is-centered plans">
+        <div
+          v-for="plan in plans"
+          :key="plan.id"
+          class="column is-4-desktop is-6-tablet is-full-mobile"
+        >
+          <plan-box :plan="plan" />
+        </div>
+      </div>
     </base-section>
-    <base-section class="is-charlie">
+    <base-section class="is-beta">
       <h1 class="title has-text-centered has-text-weight-bold">Premium Perks</h1>
       <div class="columns is-variable is-6-desktop is-6-tablet is-multiline">
         <div
@@ -20,25 +28,13 @@
         </div>
       </div>
     </base-section>
-    <base-section class="is-medium has-text-centered" id="plans">
-      <h1 class="title has-text-weight-bold mb-6">Pricing and plans</h1>
-      <div class="columns is-centered">
-        <div
-          v-for="plan in plans"
-          :key="plan.id"
-          class="column is-4-desktop is-6-tablet is-full-mobile"
-        >
-          <plan-box :plan="plan" />
-        </div>
-      </div>
-    </base-section>
-    <base-section class="is-charlie">
+    <base-section>
       <h1 class="title is-4">Notes</h1>
       <hr />
       <base-alert type="bg-info" :content="content" :button="button" />
       <ol type="1">
         <li
-          v-for="(note, index) in notes"
+          v-for="(note, index) in notes.notes"
           :key="index"
           :id="'note-' + (index + 1)"
           class="has-text-grey note"
@@ -59,8 +55,9 @@
 </template>
 
 <script>
-import perks from "@/assets/json/perks.json";
 import plans from "@/assets/json/plans.json";
+import perks from "@/assets/json/perks.json";
+import notes from "@/assets/json/notes.json";
 import faqs from "@/assets/json/faqs.json";
 import BaseSection from "@/components/BaseSection";
 import ItemCard from "@/components/ItemCard";
@@ -82,16 +79,9 @@ export default {
       perks,
       plans,
       faqs,
-      notes: [
-        `OverBot is tracking players skill ratings since <b>December 23rd, 2020</b>. 
-        Whenever a player runs the "/profile rating" command his profile SRs are saved.
-        Thus, the more "/profile rating" is used over time the more accurate the graph will be.`,
-        `News are taken from the <a class="has-text-primary has-text-outlined" href="https://playoverwatch.com/en-us/news/" target="_blank">official Overwatch website</a>.`,
-        `Join the <a class="has-text-primary has-text-outlined" href="https://discord.gg/8g3jnxv" target="_blank" rel="noopener">support server</a> 
-        and ask for the role to be set. The subscription is not affected by the role, it's just something extra.`,
-      ],
+      notes,
       content:
-        "It may take up to 5 minutes for the Premium to be set. If you have issues join the support server.",
+        "It may take up to 5 minutes for the Premium to be set. If you encounter any issues join the support server.",
       button: {
         link: "https://discord.com/invite/8g3jnxv",
         action: "Join now",
@@ -108,5 +98,9 @@ export default {
   &:not(:last-of-type) {
     margin-bottom: 1rem !important;
   }
+}
+
+.plans {
+  margin-top: 4rem !important;
 }
 </style>
