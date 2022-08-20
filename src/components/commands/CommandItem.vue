a0072d75a2fdde60e7f4c6753749e810d874962a
davidetacchini <davidetacchini11@gmail.com>
2022-08-20 17:18:05 +0200

    update tags and premium boxes style

--- src/components/commands/CommandItem.vue
+++ src/components/commands/CommandItem.vue
@@ -2,8 +2,8 @@
   <div :class="['box command p-4', { 'is-premium': command.is_premium }]">
     <h1 class="command__title title is-5">{{ command.name }}</h1>
     <h2 class="command__description subtitle mb-2">{{ command.description }}</h2>
-    <div v-if="command.guild_only" class="tag">
-      <p class="has-text-grey">Server only</p>
+    <div v-if="command.guild_only" class="tag bg-white">
+      <span>Server only</span>
     </div>
   </div>
 </template>
@@ -24,24 +24,19 @@ h2 {
   margin: 0 !important;
 }
 
-.tag {
-  background-color: transparent !important;
-  border: 1px solid $color-gamma !important;
-}
-
 .command {
   position: relative;
 
   &.is-premium {
-    box-shadow: 0 0 8px $danger !important;
+    box-shadow: 0 0 4px $info !important;
 
     &::after {
       content: "PREMIUM";
       position: absolute;
       right: 0.5rem;
-      top: 0.5rem;
-      font-size: 0.875rem;
-      color: $danger;
+      top: 0.3rem;
+      font-size: 0.8rem;
+      color: $info;
     }
   }
 
