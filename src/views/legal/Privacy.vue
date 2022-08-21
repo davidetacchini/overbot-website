25fa3c593cf139fc12c512b8206507146cfb516e
davidetacchini <davidetacchini11@gmail.com>
2022-08-21 16:39:25 +0200

    lint

--- src/views/legal/Privacy.vue
+++ src/views/legal/Privacy.vue
@@ -7,12 +7,16 @@
         <p>Last update on August 21, 2022.</p>
         <br />
         <p>By using our Website/Bot, you agree that you have read and agree to this policy.</p>
-        <h1 class="title is-5 block">What information do we collect from users that use OverBot?</h1>
+        <h1 class="title is-5 block">
+          What information do we collect from users that use OverBot?
+        </h1>
         <div class="content">
           <ul>
             <li>Public information about Servers (ID, channel IDs).</li>
             <li>Public information about Members (ID).</li>
-            <li>Public information about Overwatch profiles (platform, username, skill ratings).</li>
+            <li>
+              Public information about Overwatch profiles (platform, username, skill ratings).
+            </li>
           </ul>
           <p>Some information listed above may only be stored when specific commands are runned.</p>
         </div>
