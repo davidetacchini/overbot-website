58ec1a67a0a14f18230b6543ae60949955cca62d
davidetacchini <davidetacchini11@gmail.com>
2023-02-18 18:39:26 +0100

    dynamic baseURL based on env mode

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,15 @@
 import axios from "axios";
 
+const getBaseUrl = () => {
+  if (process.env.NODE_ENV === "production") {
+    return "https://overbot-api.vercel.app/api";
+  } else {
+    return "http://127.0.0.1:5001/api";
+  }
+};
+
 const api = axios.create({
-  baseURL: "https://overbot-api.vercel.app/api",
+  baseURL: getBaseUrl(),
   headers: {
     "Content-Type": "application/json",
   },
