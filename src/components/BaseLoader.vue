8fd99c49e2f9895365fadd45fe0672e7ea0408c3
davidetacchini <davidetacchini11@gmail.com>
2023-04-02 13:50:49 +0200

    add self closing tags

--- src/components/BaseLoader.vue
+++ src/components/BaseLoader.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="column">
     <div class="spinner">
-      <div class="double-bounce1" />
-      <div class="double-bounce2" />
+      <div class="double-bounce1"></div>
+      <div class="double-bounce2"></div>
     </div>
   </div>
 </template>
